9e76f096491eac09b29467c492ba094f8db8244c
TextBox <textbox@textbox.(none)>
2014-02-18 10:16:42 -0800

    Lowercase Capybara in env.rb. Also removed some OSX Maverick specific code

--- Cucumber_Capybara/features/support/env.rb
+++ Cucumber_Capybara/features/support/env.rb
@@ -1,39 +1,8 @@
-require "Capybara"
-require "Capybara/cucumber"
+require "capybara"
+require "capybara/cucumber"
 require "rspec"
 require 'capybara/poltergeist'
 
-module Capybara::Poltergeist
-  class Client
-    private
-    def redirect_stdout
-      prev = STDOUT.dup
-      prev.autoclose = false
-      $stdout = @write_io
-      STDOUT.reopen(@write_io)
-
-      prev = STDERR.dup
-      prev.autoclose = false
-      $stderr = @write_io
-      STDERR.reopen(@write_io)
-      yield
-    ensure
-      STDOUT.reopen(prev)
-      $stdout = STDOUT
-      STDERR.reopen(prev)
-      $stderr = STDERR
-    end
-  end
-end
- 
-class WarningSuppressor
-  class << self
-    def write(message)
-      if message =~ /QFont::setPixelSize: Pixel size <= 0/ || message =~/CoreText performance note:/ then 0 else puts(message);1;end
-    end
-  end
-end
- 
 Capybara.default_driver = :poltergeist
 Capybara.register_driver :poltergeist do |app|
     options = {
